fd82c58339045fe3ccd9cb3bb894fc0819dca024
mmf1982 <5464@gmx.net>
2021-08-24 18:18:22 +0200

    changed function to be an object and wrapped that in the original function to keep it alive without outside package reference

--- add_interactivity.py
+++ add_interactivity.py
@@ -5,96 +5,84 @@ import numpy as np
 from matplotlib.widgets import TextBox
 coords = None
 legn = None
+mlist = []
 
-def add_interactivity(legend=None, lines=None, fig=None, lines2=None, ncol=1, loc=0, ax=None, nodrag=False, legsize=12):
-    """
-    Function to add basic interactivity to an axes
-
-    - with any button pressed on the legend outside a legend line, drag and drop
-    - press the left mouse button on top of a legend line to toggle it off/ on
-    - down/ up while pressing left button, de-/increases marker size/ line width
-    - right mouse button on top of legend line (not label) to bring it to front
-    - middle button on top of legend line to open textbox to enter line name
-
-    Parameters:
-    ------------
-    (legend: legend handle
-        a specific already existing legend can be passed. If none, a new legend
-        is created)
-    (lines: list of line handles
-        a list of line handles corresponding to the legend. If None, all lines
-        that are currently present in the plot are extracted. This can lead to
-        problems if the legend that is passed does not contain all lines that
-        are extracted)
-    (fig: figure handle
-        if None, the figure is assumed to be the figure of the axes supplied, or
-        if no axis is supplied, the figure of the current active axis)
-    (lines2: list of line handles
-        instead of binding a legend to the lines in a figure, lines from another
-        axis are made clickable)
-    (ncol: integer
-        number of columns in which to distribute the legend)
-    (loc: integer
-        passed to the legend creation. 0 for "best", see matplotlib.pylot.legend
-        "loc" for help on parameters.
-    (ax: axis handle
-        axes to make interactive
-    (nodrag: bool
-        enable/ disable draggable legend
-    (legsize: int
-        font size of legend label
-
-    """
-    if ax is None:
-        ax = plt.gca()
-    if fig is None:
-        fig = ax.figure
-    if lines is None:
-        lines = ax.get_lines()
-        all_indices = []
-        for iidd, line in enumerate(lines):
-            if len(line.get_data()[0]) == 0 and not line.get_visible():
-                all_indices.append(iidd)
-        for in_iidd in all_indices[::-1]:
-            _ = lines.pop(in_iidd)
-    for line in lines:
-        if line.get_label()[0] == "_":
-            line.set_label(line.get_label()[1:])
-    if legend is None:
-        legend = ax.legend(loc=loc, ncol=ncol, prop={"size": legsize})
-    if not nodrag:
-        if int(matplotlib.__version__[0]) >= 3:
-            legend.set_draggable(True)
+class add_interactivity_class():
+    def __init__(self, legend=None, lines=None, fig=None, lines2=None, ncol=1, loc=0, ax=None, nodrag=False, legsize=12):
+        self.ncol = ncol
+        self.loc = loc
+        self.nodrag = nodrag
+        self.legsize = legsize
+        if ax is None:
+            ax = plt.gca()
+        self.ax = ax
+        self.legend, self.lines, self.linedic = self._setup(legend, lines, lines2, ncol, loc, ax, nodrag, legsize)
+        if fig is None:
+            self.fig = ax.figure
         else:
-            legend.draggable(True)
-    linedic = {}
-    for legline, line, text in zip(legend.get_lines(), lines, legend.get_texts()):
-        if legline.get_linestyle() == "None":
-            legline.set_linewidth(0)
-            legline.set_linestyle("-")
-        linedic[legline] = (line, text)
-        if float(".".join(matplotlib.__version__.split(".")[:2])) < 3.3:
-            legline.set_picker(5)
-        else:
-            legline.set_picker(True)
-            legline.set_pickradius(5)
-    if lines2 is not None:
-        for line2, line in zip(lines2, lines):
-            linedic[line2] = (line, " ")
-            if float(".".join(matplotlib.__version__.split(".")[:2])) < 3.3:
-                legline.set_picker(5)
+            self.fig = fig
+        _ = self.fig.canvas.toolbar.actions()[7].triggered.connect(self.renew)
+        _ = self.fig.canvas.mpl_connect('pick_event', self.onpick)
+        if not "update" in self.fig.canvas.toolbar.actions()[-1].text():
+            _ = self.fig.canvas.toolbar.addAction("update")
+        _ = self.fig.canvas.toolbar.actions()[-1].triggered.connect(self.renew)
+
+    def _setup(self, legend, lines, lines2, ncol, loc, ax, nodrag, legsize):
+        if lines is None:
+            lines = ax.get_lines()
+            all_indices = []
+            for iidd, line in enumerate(lines):
+                if len(line.get_data()[0]) == 0 and not line.get_visible():
+                    all_indices.append(iidd)
+            for in_iidd in all_indices[::-1]:
+                _ = lines.pop(in_iidd)
+        for line in lines:
+            if line.get_label()[0] == "_":
+                line.set_label(line.get_label()[1:])
+        if legend is None:
+            legend = ax.legend(loc=loc, ncol=ncol, prop={"size": legsize})
+        if not nodrag:
+            if int(matplotlib.__version__[0]) >= 3:
+                legend.set_draggable(True)
             else:
-                legline.set_picker(True)
-                legline.set_pickradius(5)
+                legend.draggable(True)
+        linedic = {}
+        for legline, line, text in zip(legend.get_lines(), lines, legend.get_texts()):
+            if legline.get_linestyle() == "None":
+                legline.set_linewidth(0)
+                legline.set_linestyle("-")
+            linedic[legline] = (line, text)
+            if not legline.get_picker():
+                if float(".".join(matplotlib.__version__.split(".")[:2])) < 3.3:
+                    legline.set_picker(5)
+                else:
+                    legline.set_picker(True)
+                    legline.set_pickradius(5)
+        if lines2 is not None:
+            for line2, line in zip(lines2, lines):
+                linedic[line2] = (line, " ")
+                if float(".".join(matplotlib.__version__.split(".")[:2])) < 3.3:
+                    legline.set_picker(5)
+                else:
+                    legline.set_picker(True)
+                    legline.set_pickradius(5)
+        return legend, lines, linedic
 
-    def onpick(event):
+    def renew(self, event=None):
+        self.legend, self.lines, self.linedic = self._setup(None, None, None, self.ncol, self.loc, self.ax, self.nodrag, self.legsize)
+        self.fig.canvas.draw()
+
+    def onpick(self, event):
         """
         function to be called on click (left, middle, right) on an artist
         """
+        fig = event.artist.figure
+        ax = event.artist.axes
+        legend = ax.get_legend()
         leg = event.artist
         # print(event, event.mouseevent.button, leg, leg.get_label())
         try:
-            (plotline, mtext) = linedic[leg]
+            (plotline, mtext) = self.linedic[leg]
             isline = True
         except:
             isline = False
@@ -102,7 +90,7 @@ def add_interactivity(legend=None, lines=None, fig=None, lines2=None, ncol=1, lo
             mtext = None
         if event.mouseevent.button == 3 and isline:
             maxordernow = np.nanmax(
-                np.array([linedic[ll][0].get_zorder() for ll in linedic]))
+                np.array([self.linedic[ll][0].get_zorder() for ll in self.linedic]))
             plotline.set_zorder(maxordernow + 1)
             legend.set_zorder(maxordernow + 2)
         elif event.mouseevent.button == 1 and isline:
@@ -154,7 +142,7 @@ def add_interactivity(legend=None, lines=None, fig=None, lines2=None, ncol=1, lo
                     else:
                         leg.set_linewidth(0)
                         plotline.set_linewidth(0)
-        elif event.mouseevent.button == 2 and isline and not nodrag:
+        elif event.mouseevent.button == 2 and isline:
             print("Type the new label in the box")
             def submit(stext):
                 print("you typed", stext)
@@ -183,7 +171,11 @@ def add_interactivity(legend=None, lines=None, fig=None, lines2=None, ncol=1, lo
                     for i in range(len(legend.texts)):
                         legend.texts[i].set_fontsize(legend.texts[i].get_fontsize()+1)
         fig.canvas.draw()
-    _ = fig.canvas.mpl_connect('pick_event', onpick)
+
+def add_interactivity(*args, **kwargs):
+    global mlist
+    element = add_interactivity_class(*args, **kwargs)
+    mlist.append(element)
 
 def add_ai_toall():
     '''
@@ -193,47 +185,15 @@ def add_ai_toall():
         mfig = plt.figure(mfignum)
         for ax in mfig.axes:
             add_interactivity(fig=mfig, ncol=1, loc=0, ax=ax, nodrag=False, legsize=10)
+    return
 
-def enable_copy_paste(figs=None, legsize=10):
-    '''
-    Function to call after plots are created to be able to copy paste line plots
-
-    A pick event on lines is activated and will copy the line data if 'c' is
-    pressed when the line is selected.
-
-    Clicking in any axes that was already present before this function had been
-    called, and clicking again while pressing 'v' will add the currently copied
-    plot
-    '''
-    print("to copy a line, click the right mouse button on a line")
-    print("to paste, press the middle mouse button in an axes")
-    print("to delete a line, double click on it")
-    global coords, legn
-    def update_components(figs=None):
-        if figs is None:
-            figl = plt.get_fignums()
-            print(figl)
-            figs = []
-            for fi in figl:
-                figs.append(plt.figure(fi))
-        for mfig in figs:
-            print(mfig)
-            axs = mfig.axes
-            print(axs)
-            for ax in axs:
-                for lin in ax.lines:
-                    if not lin.get_picker():
-                        if float(".".join(matplotlib.__version__.split(".")[:2])) < 3.3:
-                            lin.set_picker(5)
-                        else:
-                            lin.set_picker(True)
-                            lin.set_pickradius(5)
-                    else:
-                        print(lin, " already has picker")
-            d = mfig.canvas.mpl_connect("pick_event", onpick)
-            f = mfig.canvas.mpl_connect('button_press_event', onclick)
+def cp_one(ax, mfig):
+    def update_self(event):
+        #mfig = event.canvas.figure
+        #print(ax,mfig)
+        update_components(ax, mfig)
     def onpick(event):
-        global coords, legn, artist
+        global coords, legn, artist, ax
         artist = event.artist
         if event.mouseevent.button == 3 and type(artist) != matplotlib.legend.Legend:
             try:
@@ -249,30 +209,79 @@ def enable_copy_paste(figs=None, legsize=10):
         elif event.mouseevent.dblclick and type(artist) != matplotlib.legend.Legend:
             try:
                 ax = artist.axes
+                # print("axes figure: ", ax, artist)
                 artist.remove()
-                add_interactivity(ax=ax, legsize=legsize)
+                #add_interactivity(ax=ax, legsize=legsize)
                 ax.figure.canvas.draw()
-                print("line removed")
             except NotImplementedError:
                 pass
             except ValueError as verr:
-                print(verr)
+                pass
+                #print(verr)
         else:
             pass
     def onclick(event):
         global coords, legn
         if event.button == 2:
             if legn is not None:
-                print("pasted ", legn)
-                print("coordrange x: ", np.nanmin(coords[0]), np.nanmax(coords[0]))
-                print("coordrange y: ", np.nanmin(coords[1]), np.nanmax(coords[1]))
+                # print("pasted ", legn)
+                # print("coordrange x: ", np.nanmin(coords[0]), np.nanmax(coords[0]))
+                # print("coordrange y: ", np.nanmin(coords[1]), np.nanmax(coords[1]))
                 ax = event.inaxes
                 ax.plot(coords[0], coords[1], label=legn)
-                update_components(figs=figs)
-                add_interactivity(ax=ax, legsize=legsize)
+                update_components(ax, mfig)
+                # add_interactivity(ax=ax, legsize=legsize)
                 ax.figure.canvas.draw()
                 legn = None
-    update_components(figs=figs)
+    def update_components(ax, mfig):
+        for lin in ax.lines:
+            if not lin.get_picker():
+                if float(".".join(matplotlib.__version__.split(".")[:2])) < 3.3:
+                    lin.set_picker(5)
+                else:
+                    lin.set_picker(True)
+                    lin.set_pickradius(5)
+            else:
+                pass
+                #print(lin, " already has picker")
+    d = mfig.canvas.mpl_connect("pick_event", onpick)
+    f = mfig.canvas.mpl_connect('button_press_event', onclick)
+    update_components(ax, mfig)
+    mfig.canvas.toolbar.actions()[7].triggered.connect(update_self)
+    mfig.canvas.mpl_connect('draw_event', update_self)
+    return d, f
+
+def enable_copy_paste(figs=None):
+    '''
+    Function to call after plots are created to be able to copy paste line plots
+
+    A pick event on lines is activated and will copy the line data if 'c' is
+    pressed when the line is selected.
+
+    Clicking in any axes that was already present before this function had been
+    called, and clicking again while pressing 'v' will add the currently copied
+    plot
+    '''
+    print("to copy a line, click the right mouse button on a line")
+    print("to paste, press the middle mouse button in an axes")
+    print("to delete a line, double click on it")
+    mlist = []
+    if figs is None:
+        figl = plt.get_fignums()
+        figs = []
+        for fi in figl:
+            figs.append(plt.figure(fi))
+    for mfig in figs:
+        axs = mfig.axes
+        for ax in axs:
+            a,b = cp_one(ax, mfig)
+            mlist.append(a)
+            mlist.append(b)
+    return mlist
+
+def clear_all():
+    global mlist
+    mlist = []
 
 def interactive():
     '''
@@ -281,12 +290,13 @@ def interactive():
     enable_copy_paste()
     add_ai_toall()
 
-def main():
+def main(notext=False):
     fig, ax = plt.subplots(1,2)
     ax[0].plot(np.arange(10), lw=6)
     ax[0].plot(np.arange(10) ** 1.5, 'o', ms=12)
     ax[0].plot(np.sin(np.arange(10)) * 5, lw=6)
-    print(
+    if not notext:
+        print(
         "This is a simple plot to which interactivity has been added:\n",
         " \n",
         "* with any button pressed on the legend outside a legend line, you can drag and drop the legend\n",
@@ -300,7 +310,8 @@ def main():
         "* a right mouse click on a line in the left plot copies the data.\n",
         "* click middle button in the right plot to paste it.\n"
         "* double click a line to remove it from the plot")
-    add_interactivity(ax=ax[0])
+    mlist = add_ai_toall()
+    #add_interactivity(ax=ax[1])
     enable_copy_paste()
     plt.show()
 
